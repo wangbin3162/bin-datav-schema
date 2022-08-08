a29d6a1eb4b7afa250df821925b1d26bfe14ccae
wangbin <316281400@qq.com>
2022-08-08 16:36:43 +0800

    发布新版预览

--- docs/static/js/index-66ac20e2.js
+++
@@ -1,1 +0,0 @@
-import{at as n,aE as c,a1 as p,o as u,a as i}from"./vendor-471160ad.js";import"./chunk-brace-269ffce0.js";const _={name:"Redirect",setup(){const{currentRoute:r,replace:t}=c(),{params:a,query:o}=p(r),{path:e}=a,s=Array.isArray(e)?e.join("/"):e;return t({path:"/"+s,query:o}),{}}};function d(r,t,a,o,e,s){return u(),i("div")}var l=n(_,[["render",d]]);export{l as default};
